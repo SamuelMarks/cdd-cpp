d6e3a2aa173d05fdebb7116b3f6609a5d8692bf9
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-04-12 19:09:14 +1200

    chore: update gitignore and track exception stub

---
+++ src/cli/exceptions_stub.cpp
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+extern "C" {
+    void* __cxa_allocate_exception(size_t thrown_size) noexcept { abort(); return nullptr; }
+    void __cxa_free_exception(void *thrown_exception) noexcept { abort(); }
+    void __cxa_throw(void *thrown_exception, void *tinfo, void (*dest)(void *)) { abort(); }
+    void* __cxa_begin_catch(void *exceptionObject) noexcept { abort(); return nullptr; }
+    void __cxa_end_catch() { abort(); }
+    void __cxa_rethrow() { abort(); }
+}
